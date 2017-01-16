0f24b83dd0ab3fff3b0eaa0da3571e75f2fa10d8
Julian Brackins <julian.brackins@mines.sdsmt.edu>
2017-01-16 01:04:05 -0700

    methods for populating planet class and tile class from xml

--- timaps/parse.py
+++ timaps/parse.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 import sys
+from planet import Planet
+from tile   import SystemTile
 
 class XMLParser:
     """XML Parser Methods
@@ -33,6 +35,48 @@ class XMLParser:
 
     def getRoot(self):
         return self.tree.getroot()
+
+    def getPlanets(self, tile):
+        lst = []
+        for planet in tile.findall('planet'):
+            p = Planet()
+
+            n   = planet.attrib['name']
+            d   = planet.attrib['description']
+            res = int(planet.attrib['resources'])
+            inf = int(planet.attrib['influence'])
+            r   = int(planet.attrib['red'])
+            g   = int(planet.attrib['green'])
+            b   = int(planet.attrib['blue'])
+            y   = int(planet.attrib['yellow'])
+
+            p.setName(n)
+            p.setDescription(d)
+            p.setStats(res,inf)
+            p.setTech(r,g,b,y)
+
+            # add planet to list
+            lst.append(p) 
+
+        # return list of planets in the system
+        return lst
+
+    def getTile(self,tile):
+        newTile = SystemTile()
+        for color in tile.findall('color'):
+            newTile.setColor(color.text)
+        for systype in tile.findall('type'):
+            newTile.setType(systype.text)
+        for prop in tile.findall('property'):
+            newTile.setProperty(prop.text)
+        for gates in tile.findall('gates'):
+            newTile.setGates(str(gates.text))
+        for race in tile.findall('race'):
+            newTile.setRace(str(race.text))
+        planets = self.getPlanets(tile)
+        newTile.setPlanets(planets)
+        return newTile
+            
 
     def test(self):
         count = 0
